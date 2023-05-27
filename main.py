75ed0e3a16ac8e8d6b7612af6b79987294dd6c6f
ryane-joe <ryanejoe@proton.me>
2023-05-27 20:35:21 +0530

    Added proper login along with exceptions for login, added function to test gathering of basic user info

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import instaloader
-def login(usrname_attk,passwd_attk):
+
+def login(username, password):
     try:
-        instaloader.Instaloader().login(usrname_attk,passwd_attk)
-    except:
-        tfa_code_attk = input('enter 2fa code:')
-        instaloader.Instaloader.two_factor_login(tfa_code_attk)
+        loader = instaloader.Instaloader()
+        loader.login(username, password)
+        print("Login successful!")
+    except instaloader.exceptions.TwoFactorAuthRequiredException:
+        tfa_code = input("Enter 2FA code: ")
+        loader.two_factor_login(tfa_code)
+        print("2FA login successful!")
+    except instaloader.exceptions.BadCredentialsException:
+        print("Invalid username or password.")
+
+# Usage
+username = "ryanesalt"
+password = "Jacob2755"
+
+login(username, password)
+loader = instaloader.Instaloader()
+profile = instaloader.Profile.from_username(loader.context, 'ryanejoe_')
+print("Username: ", profile.username)
+print("User ID: ", profile.userid)
+print("Number of Posts: ", profile.mediacount)
+print("Followers Count: ", profile.followers)
+print("Following Count: ", profile.followees)
+print("Bio: ", profile.biography)
+print("External URL: ", profile.external_url)
