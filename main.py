b70782fda8c7caa21a1de06fd9c942f6fcc36951
ryane-joe <ryanejoe@proton.me>
2023-05-28 01:04:39 +0530

    removed the comment which i actually never implemented becz i realised its a bad idea

--- main.py
+++ main.py
@@ -12,16 +12,6 @@ except instaloader.exceptions.TwoFactorAuthRequiredException:
     print("2FA login successful!")
 except instaloader.exceptions.BadCredentialsException:
     print("Invalid username or password.")
-#Since im lazy and dont wanna work w dictionaries, and since its gonna be simply 7 elems of data i work w.
-'''
-0:username
-1:userID
-2:Mediacount
-3: followers
-4: followees
-5.Bio
-6:external url
-'''
 def get_basic_info(usrname_tar):
     profile = instaloader.Profile.from_username(loader.context, usrname_tar)
     comp_data = {'username:':profile.username,'user_id':profile.userid,'media_count':profile.mediacount,'follower_count' : profile.followers,'following_count':profile.followees,'bio':profile.biography,'external_url': profile.external_url}
