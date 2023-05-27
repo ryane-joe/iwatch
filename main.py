2b7611b5ecbb9d6c3cc60becb023d69b9e1f8679
ryane-joe <ryanejoe@proton.me>
2023-05-27 22:24:33 +0530

    login working, get_basic_info() does successfully load all comparison information onto the dictionary and returns it, temp prints out the dict info for testing

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import instaloader
 
-def login(username, password):
-    try:
-        loader = instaloader.Instaloader()
-        loader.login(username, password)
-        print("Login successful!")
-    except instaloader.exceptions.TwoFactorAuthRequiredException:
-        tfa_code = input("Enter 2FA code: ")
-        loader.two_factor_login(tfa_code)
-        print("2FA login successful!")
-    except instaloader.exceptions.BadCredentialsException:
-        print("Invalid username or password.")
-
-# Usage
-username = "ryanesalt"
-password = "Jacob2755"
-
-login(username, password)
-loader = instaloader.Instaloader()
-profile = instaloader.Profile.from_username(loader.context, 'ryanejoe_')
-print("Username: ", profile.username)
-print("User ID: ", profile.userid)
-print("Number of Posts: ", profile.mediacount)
-print("Followers Count: ", profile.followers)
-print("Following Count: ", profile.followees)
-print("Bio: ", profile.biography)
-print("External URL: ", profile.external_url)
+username = input('enter username:')
+password = input('enter password:')
+try:
+    loader = instaloader.Instaloader()
+    loader.login(username, password)
+    print("Login successful!")
+except instaloader.exceptions.TwoFactorAuthRequiredException:
+    tfa_code = input("Enter 2FA code: ")
+    loader.two_factor_login(tfa_code)
+    print("2FA login successful!")
+except instaloader.exceptions.BadCredentialsException:
+    print("Invalid username or password.")
+#Since im lazy and dont wanna work w dictionaries, and since its gonna be simply 7 elems of data i work w.
+'''
+0:username
+1:userID
+2:Mediacount
+3: followers
+4: followees
+5.Bio
+6:external url
+'''
+def get_basic_info(usrname_tar):
+    profile = instaloader.Profile.from_username(loader.context, usrname_tar)
+    comp_data = {'username:':profile.username,'user_id':profile.userid,'media_count':profile.mediacount,'follower_count' : profile.followers,'following_count':profile.followees,'bio':profile.biography,'external_url': profile.external_url}
+    return comp_data
+print(get_basic_info('usrname'))
