111e353d47bbe3ef2813d04d1b4db9e51b88c271
ryane-joe <ryanejoe@proton.me>
2023-05-29 09:32:26 +0530

    added file compatibility, checking for changes, printing changes, file writing. UN-TESTED. retrive_comp_data() (for retriving the saved comparison data from the save file) is NOT implemented

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import instaloader
+import csv
+import os
+import time
 usr = input('enter target username:')
 username = input('enter username:')
 password = input('enter password:')
@@ -23,18 +26,61 @@ def download_posts_and_reels(username):
     posts = profile.get_posts()
     for index, post in enumerate(posts,1):
         loader.download_post(post, target=f"{profile.username}_{index}")
-
+def check_for_changes(comp_data,newdata,poss):
+    newdata = get_basic_info(loader, username)
+    if newdata != comp_data:
+        print("Changes detected:")
+        for key in newdata:
+            if newdata[key] != comp_data[key]:
+                print(f"{key}: {comp_data[key]} -> {newdata[key]}")
+                if poss == True and key == 'media_count':
+                    download_posts_and_reels(usr)
+def write_file(fil, co,new):
+    header = ['username','userID','media_count','follower_count','following_count','bio','externalurl']
+    if new == True:
+        with open(fil,'w') as compE:
+            for i in range(6):
+                writer = csv.writer(compE)
+                writer.writerow(header)
+                writer.writerows(co)
+    else:
+        with open(fil,'a') as compE:
+            for i in range(6):
+                writer = csv.writer(compE)
+                writer.writerow(header)
+                writer.writerows(co)
+def Latest_data_show(comp_data):
+    print('LATEST DATA')
+    print('username:',comp_data['username'])
+    print('userID:',comp_data['user_id'])
+    print('mediacount',comp_data['media_count'])
+    print('follower count:',comp_data['follower_count'])
+    print('following count:',comp_data['following_count'])
+    print('bio:',comp_data['bio'])
+    print('external link:',comp_data['external_url'])
+def retrive_comp_data(filen):
+    pass
 while True:
-    if "comp_data" not in globals():
-        comp_data = get_basic_info(usr)
-        print('recived original data')
-        print('username:',comp_data['username'])
-        print('userdata:',comp_data['user_id'])
-        print('mediacount',comp_data['media_count'])
-        print('follower count:',comp_data['follower_count'])
-        print('following count:',comp_data['following_count'])
-        print('bio:',comp_data['bio'])
-        print('external link:',comp_data['external_url'])
-    elif "comp_data" in globals():
-        print('not implemented')
-        break
+    if os.path.isfile('comp__'):
+        m = get_basic_info(usr)
+        print('LATEST DATA:')
+        Latest_data_show(m)
+        comp_data = retrive_comp_data('comp__')
+        if m != comp_data:
+            check_for_changes(comp_data,m)
+            print('updated comparison data')
+            write_file('comp__',m,False)
+            print('updated comp file')
+        else:
+            print('no changes detected')
+    else:
+        m = get_basic_info(usr)
+        print('detected no comparision save file')
+        Latest_data_show(m)
+        write_file('comp__',m,True)
+        print('written new comparsion data file')
+    print('monitoring loop started')
+    time.sleep(60)
+
+
+    
